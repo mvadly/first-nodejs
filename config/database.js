d9ce728a515a26cbccb7b8a10c6082f9c0e67025
Muhammad <mfadly24@gmail.com>
2023-05-02 12:23:20 +0700

    update

--- config/database.js
+++ config/database.js
@@ -15,9 +15,8 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 //   }
 // }
 
-const mdb = async() => {
-  const uri =
-    "mongodb+srv://api-mvadly:Pa55w0rd123!!@dashboard.yzsvlox.mongodb.net/?retryWrites=true&w=majority";
+const mdb = async () => {
+  const uri = process.env.MONGODB_URI;
   // Create a MongoClient with a MongoClientOptions object to set the Stable API version
   const client = new MongoClient(uri, {
     serverApi: {
