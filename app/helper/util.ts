44e0eca992fd393d227630ee859464e16fa7d3f4
Muhammad <mfadly24@gmail.com>
2023-05-03 16:11:33 +0700

    update datetime

--- app/helper/util.ts
+++ app/helper/util.ts
@@ -1,7 +1,7 @@
 import moment from "moment-timezone";
 
 const getDateTime = () => {
-  return moment().tz("Asia/Jakarta").format("YYYY-MM-DD HH:mm:ss ZZ");
+  return moment().tz("Asia/Jakarta").format();
 };
 const util: Object = {
   getDateTime,
