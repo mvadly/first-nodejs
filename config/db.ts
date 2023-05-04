40dedc37b5545251d5d4dace46d83c0f59e66dcb
Muhammad <mfadly24@gmail.com>
2023-05-04 23:17:20 +0700

    update

--- config/db.ts
+++ config/db.ts
@@ -1,13 +1,15 @@
 import mongoose, { ConnectOptions } from "mongoose";
 const callMongo = async () => {
-     await mongoose.connect(process.env.MONGODB_URI!, {
+    mongoose.set('strictQuery', false);
+
+    await mongoose.connect(process.env.MONGODB_URI!, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     } as ConnectOptions).then(() => {
         console.log('Connected to MongoDB');
     }).catch((err) => {
         console.error('Error connecting to MongoDB', err);
-        process.exit
+        process.exit(1)
     });
 }
 
